e31c223f53f4752634736b19cc0365a28661dd42
alicjapol <alicjaxpoltorak@gmail.com>
2024-01-18 18:42:48 +0100

    section 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
-import Profile from './profile/Profile.jsx'
+import Profile from './profile/Profile.jsx';
 import css from './profile/Profile.module.css';
-
+import Statistics from './statistics/Statistics.jsx';
+import data from '../data/data.json';
 import user from '../data/user.json';
 export const App = () => {
-
   return (
     <div className="App">
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-  </div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+
+    </div>
   );
 };
