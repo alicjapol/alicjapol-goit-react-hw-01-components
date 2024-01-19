9f21727d66198cb4dbfa6f47a577a2c2e8f42810
alicjapol <alicjaxpoltorak@gmail.com>
2024-01-19 19:08:55 +0100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import Profile from './TransactionHistory/Profile.jsx';
-import Statistics from './TransactionHistory/Statistics.jsx';
+import Profile from './Profile/Profile.jsx';
+import Statistics from './Statistics/Statistics.jsx';
 import FriendList from './FriendList/FriendList.jsx';
 import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
 import data from '../data/data.json';
